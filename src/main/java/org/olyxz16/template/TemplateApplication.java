d2fecfc86422be49722f1ee697811cd03e439625
Olyxz16 <cedric.sazos@tutanota.com>
2025-12-23 12:51:06 +0100

    Init commit

---
+++ src/main/java/org/olyxz16/template/TemplateApplication.java
@@ -0,0 +1,13 @@
+package org.olyxz16.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/olyxz16/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.olyxz16.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
